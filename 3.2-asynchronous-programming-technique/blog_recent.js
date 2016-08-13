48370795286a30fee3e644a91b23dda7891d6dd6
Soyoun Kim <makeajourney@gmail.com>
2016-08-13 20:33:59 +0900

    3.7 중간 함수 생성을 이용한 중첩 줄이기의 예

--- 3.2-asynchronous-programming-technique/blog_recent.js
+++ 3.2-asynchronous-programming-technique/blog_recent.js
@@ -5,30 +5,39 @@
 var http = require('http');
 var fs = require('fs');
 
-http.createServer(function(req, res) {      // HTTP Server를 생성하고 응답 로직을 정의한 콜백을 사용
-    if (req.url == '/') {
-        fs.readFile('./title.json', function(arr, data) {      // JSON 파일을 읽고 파일 내용을 처리할 방법을 정의한 콜백을 사용
-            if (err) {
-                console.error(err);
-                res.end('Server Error');
-            }
-            else {
-                var titles = JSON.parse(data.toString());       // JSON 텍스트를 파싱
-
-                fs.readFile('./template.html', function(err, data) {        // HTML 템플릿을 읽고 템플릿이 로드됐을 때 콜백 사용
-                    if (err) {
-                        console.err(err);
-                        res.end('Server error');
-                    }
-                    else {
-                        var tmpl = data.toString();
-
-                        var html = tmpl.replace('%', titles.join('</li><li>'));     // 블로그 글의 제목을 보여줄 HTML 페이지 조합
-                        res.writeHead(200, {'Content-Type': 'text/html'});
-                        res.end(html);      // 사용자에게 HTML 페이지 전송
-                    }
-                });
-            }
-        });
-    }
+var server = http.createServer(function (req, res) {    // 클라이언트 요청의 최초 진입점
+    getTitles(res);     // getTitles로 제어를 넘김
 }).listen(8000, "127.0.0.1");
+
+function getTitles(res) {   // getTitles에서 제목을 가져오고 getTemplate에 제어를 넘김
+    fs.readFile('./titles.json', function (err, data) {     // JSON 파일을 읽고 파일 내용을 처리할 방법을 정의한 콜백을 사용
+        if (err) {
+            hadError(err, res);
+        }
+        else {
+            getTemplate(JSON.parse(data.toString()), res);      // JSON 텍스트를 파싱해서 getTemplate로 넘김
+        }
+    });
+}
+
+function getTemplate(titles, res) {     // getTemplate에서 템플릿 파일을 읽은 후 formatHtml에 제어를 넘김
+    fs.readFile('./template.html', function (err, data) {       // HTML 템플릿을 읽고 템플릿이 로드됐을 때 콜백 사용
+        if (err) {
+            hadError(err, res);
+        }
+        else {
+            formatHtml(titles, data.toString(), res);
+        }
+    });
+}
+
+function formatHtml(titles, tmpl, res) {        // formatHtml은 제목과 템플릿으로 클라이언트에 응답할 페이지를 렌더링
+    var html = tmpl.replace('%', titles.join('</li><li>'));     // 블로그 글의 제목을 보여줄 HTML 페이지 조합
+    res.writeHead(200, {'Content-Type': 'text/html'});
+    res.end(html);      // 사용자에게 HTML 페이지 전송
+}
+
+function hadError(err, res) {
+    console.error(err);
+    res.end('Server Error');
+}
